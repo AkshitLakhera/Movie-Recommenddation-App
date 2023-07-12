57a701a55b509e3707d43c33b9bf97b5c2c7bf92
“[Akshit <[akshitlakhera14@gmail.com]>
2023-07-12 23:28:00 +0530

    Installing boiler plate code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hello this is first React App
         </a>
       </header>
     </div>
