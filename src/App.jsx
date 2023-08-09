e63d50496960446612df34bf477e4a4315f24c26
“[Akshit <[akshitlakhera14@gmail.com]>
2023-08-09 22:36:09 +0530

    Added image carousel in react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './ components/Navbar';
+import Carousel from './ components/Carousel';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Carousel/>
     </div>
   )
 }
